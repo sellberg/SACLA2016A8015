220c2e6bea667c8ecf4c205463f52d3ef8d793c0
Jonas Sellberg <jonas.sellberg@naturalcycles.com>
2016-06-24 18:35:22 +0900

    added process_runs and process_batch_runs wrapper to Kims analysis code, renamed other scripts, fixed data_process.cpp so it works on xhpcfep

--- code/data_process.cpp
+++ code/data_process.cpp
@@ -7,19 +7,24 @@
 using namespace std;
 
 void generate_input();
-int run_number=0;
+int run_number = 0;
 
 
 
 
-int main(){
+int main(int argc, char* argv[]){
 
-    cout<<"run nunber:";
-    scanf("%d", &run_number);
+    if (argc > 1) {
+        //run_number = stoi(argv[1], nullptr, 0); // only supported in C++11 standard
+        run_number = atoi(argv[1]);
+    } else {
+        cout<<"run number: ";
+        scanf("%d", &run_number);
+    }
+    //cout << "run_number = " << run_number << endl;
     generate_input();
     system("chmod 777 one_shot");
     system("./one_shot");
-
 }
 
 
@@ -29,8 +34,9 @@ void generate_input()
     ofstream dataprocess;
     dataprocess.open("one_shot");
     dataprocess<<"MakeTagList -b 3 -r "<<run_number<<" -det 'MPCCD-1N0-M01-002, MPCCD-1N0-M01-003, MPCCD-8-2-002' -out tag_"<<run_number<<".list"<<endl;
-    dataprocess<<"DataConvert4 -l tag_"<<run_number<<".list -dir /UserData/fperakis/2016_6/run"<<run_number<<"/ -o "<<run_number<<".h5"<<endl;
-    dataprocess<<"./matlab_background_exe"<<endl;
+    //dataprocess<<"DataConvert4 -l tag_"<<run_number<<".list -dir /UserData/fperakis/2016_6/run"<<run_number<<"/ -o "<<run_number<<".h5"<<endl;
+    dataprocess<<"DataConvert4 -l tag_"<<run_number<<".list -dir ./ -o "<<run_number<<".h5"<<endl;
+    dataprocess<<"./matlab_background.csh"<<endl;
     dataprocess<<"./integ.out"<<endl;
     dataprocess<<"./av.out"<<endl;
 }
